82969b0baafa6d2253dcd0686b632ac22ba7512a
Qaasim-md <qaasimdavids1@gmail.com>
2025-10-31 10:17:55 +0200

    add exercise 4

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+let score = 70;
+let passgrade = 60;
+
+if (score == 100) {
+    console.log("perfect score");
+}
+else if (score >= passgrade) {
+    console.log("pass");
+} else {
+    console.log("fail");
+}
